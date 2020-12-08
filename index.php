306aed0f6563e928e03624ef0b27e2a2ef2d49c0
haerul2797 <anwardhaerul@gmail.com>
2020-12-08 07:54:51 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <body>
 <h1>Hello World</h1>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quos, qui illo neque quam laborum dolores recusandae alias officia sint reiciendis, eveniet enim similique consequuntur provident quod voluptatem deserunt quibusdam.</p>
+<ul>
+<li>Haerul</li>
+<li>Anwar</li>
+</ul>
     
 </body>
 </html>
